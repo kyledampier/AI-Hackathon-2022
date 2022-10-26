9ca25d9813a19de529f27832a489e10b75cc7536
Matthew Hibbs <89611778+matthibbs7@users.noreply.github.com>
2022-10-26 11:45:14 -0400

    Added login auth

--- frontend/src/components/Modal/Auth/Login.tsx
+++ frontend/src/components/Modal/Auth/Login.tsx
@@ -2,7 +2,9 @@ import { Button, Divider, Input, Text, Image, Flex } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { authModalState } from '../../../atoms/authModalAtom';
-
+import { auth } from '../../../firebase/clientApp';
+import { useSignInWithEmailAndPassword} from 'react-firebase-hooks/auth';
+import { FIREBASE_ERRORS } from '../../../firebase/error';
 const Login:React.FC = () => {
 
     const setAuthModalState = useSetRecoilState(authModalState)
@@ -12,6 +14,20 @@ const Login:React.FC = () => {
         password: '',
     })
 
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth)
+
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        console.log("fORM SUBMIT")
+        event.preventDefault();
+
+        signInWithEmailAndPassword(loginForm.email, loginForm.password)
+    }
+
     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setLoginForm(prev => ({
             ...prev,
@@ -20,7 +36,7 @@ const Login:React.FC = () => {
     }
 
     return (
-        <form onSubmit={() => {}}>
+        <form onSubmit={onSubmit}>
             <Flex width="105%" flexDirection="column">
                 <Flex align="center" flexDirection="row" justifyContent="center"> 
                     <Image src="/images/educatus_mid.png" height="54px" width="54px" />
@@ -31,8 +47,8 @@ const Login:React.FC = () => {
                 <Input _placeholder={{color: '#2B2C31'}} _focus={{border: '2px solid #616aee'}} fontWeight={700} color="white" border="0px" bg="#202125" required name="email" placeholder="Email Address" type="email" mb={2} onChange={onChange} />
                 <Text fontWeight={900} color="white">Password</Text>
                 <Input _placeholder={{color: '#2B2C31'}} _focus={{border: '2px solid #616aee'}} fontWeight={700} color="white" border="0px" bg="#202125" mb={2} required name="password" placeholder="Password" type="password" onChange={onChange} />
-                {/* <Text textAlign='center' color='red' fontSize='10pt'>TODO Firebase error goes here</Text> */}
-                <Button _focus={{border: '2px solid #616aee'}} _loading={{backgroundColor: 'teal.400'}} isLoading={false} width='100%' height='36px' mt={2} mb={2} bg="#616aee" color="white" type='submit' _hover={{bg: '#5f40f7'}}>Sign In</Button>
+                <Text textAlign='center' color='red' fontSize='10pt'>{FIREBASE_ERRORS[error?.message as keyof typeof FIREBASE_ERRORS]}</Text>
+                <Button _focus={{border: '2px solid #616aee'}}  isLoading={loading} width='100%' height='36px' mt={2} mb={2} bg="#616aee" color="white" type='submit' _hover={{bg: '#5f40f7'}}>Sign In</Button>
                 <Flex justify="center">
                     <Text color="white" fontWeight={600} textAlign="center" fontSize="9pt" mt={1}>Forgot your password?&nbsp;</Text>
                     <Text textAlign="center" fontSize="9pt" mt={1} color="#616aee" fontWeight={700} _hover={{cursor: 'pointer'}} onClick={() => setAuthModalState((prev) => ({
@@ -47,6 +63,7 @@ const Login:React.FC = () => {
                         view: 'signup',
                     }))}> Sign Up</Text>
                 </Flex>
+                
             </Flex>
             
         </form>
