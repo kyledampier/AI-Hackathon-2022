64dd959371ded0cf0e5ecf1ed3ba014aa150876e
Kyle Dampier <kyle@dampier.io>
2022-10-26 09:56:37 -0400

    added python libraries and started FastAPI server

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,27 @@
 import os
 import pandas as pd
 import numpy as np
+from fastapi import FastAPI # File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 
-print("Hello World")
+from pydantic import BaseModel
+
+app = FastAPI()
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+)
+
+@app.get("/")
+def index():
+    return {"message": "Hello Frontend"}
+
+
