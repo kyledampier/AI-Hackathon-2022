e67dba8d3e3c08fc741e7a9071319e66a5c78c08
Kyle Dampier <kyle@dampier.io>
2022-10-26 16:11:11 -0400

    Merged main.py with Gabes changes :(

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
-from fastapi import FastAPI # File, UploadFile
+from fastapi import FastAPI  # File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
@@ -27,20 +27,52 @@ app.add_middleware(
     allow_methods=["*"],
 )
 
+data = pd.read_csv('data_all.csv')
+
+
 class RewordingItem(BaseModel):
     audience: int
     text: Union[str, None] = None
 
+
+@app.get("/")
+def index():
+    return {"message": "Hello Frontend"}
+
+
 class NERItem(BaseModel):
     text: Union[str, None] = None
+
 
 class AnalogyItem(BaseModel):
     target: Union[str, None] = None
     text: Union[str, None] = None
 
+
+@app.get("/countries")
+def get_countries():
+    return data[['Country', 'Code', 'ContinentCode']].fillna('').to_dict(orient='records')
+
+
+@app.get("/columns")
+def get_columns():
+    cols = data.drop(['Country', 'Code', 'ContinentCode'], axis=1).columns
+    out = []
+    for col in cols:
+        if not col.endswith('|year'):
+            out.append(col)
+    return out
+
+
+@app.post("/compare")
+def get_country(country_code: str):
+    return data[data['Code'] == country_code].to_dict(orient='records')[0]
+
+
 @app.post("/")
 def index():
     return {"message": "Hello Frontend"}
+
 
 @app.post("/ner")
 def entity_endpoint(item: NERItem):
@@ -54,8 +86,10 @@ def analogy_endpoint(item: AnalogyItem):
     analogy = getAnalogy(item.target, item.text)
     return {"analogy": analogy}
 
-@app.post("/rewording")
-def get_rewording(item: RewordingItem):
-    if item.text:
-        reworded_text = reword(item.audience, item.text)
-    return {"reworded_text" : reworded_text}
+
+@app.get("/rewording")
+def get_rewording(audience: int, text: Union[str, None] = None):
+    if text:
+        reworded_text = reword(audience, text)
+    return {"reworded_text": reworded_text}
+
--- frontend/src/pages/compare.tsx
+++ frontend/src/pages/compare.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
-import { useState } from 'react'
-import { Flex, Text, Box, Button, Modal } from '@chakra-ui/react'
+import React, { useState, useEffect } from 'react'
+import { Flex, Text, Box, Button, Modal, Select } from '@chakra-ui/react'
 
 const input_example = [
     {
@@ -17,30 +17,84 @@ const input_example = [
     },
 ]
 
+interface Country { 
+    Country: string,
+    Code: string,
+    ContinentCode: string
+}
 
 const Test: NextPage = () => {
 
+    const [countries, setCountries] = useState<Array<Country>>([])
+    const [selectedCountries, setSelectedCountries] = useState<Array<string>>([])
+    const [columnOptions, setColumnOptions] = useState<Array<string>>([])
+    const [selectedColumns, setSelectedColumns] = useState<Array<string>>([])
+
+    useEffect(() => {
+        fetch("http://localhost:8080/countries").then(async (response) => {
+            const columns = await fetch("http://localhost:8080/columns")
+            const columnOptions = await columns.json()
+
+            response.json().then((data) => {
+                setCountries(data)
+                setColumnOptions(columnOptions)
+            })
+        })
+    }, [])
+
+    const handleSelectCountry = (e : React.ChangeEvent<HTMLFormElement>) => {
+        const val = e.target.value as string;
+        const tmp = [... selectedCountries] as Array<string>
+        tmp.push(val)
+        setSelectedCountries(tmp)
+    }
+
+    const handleSelectColumn = (e : React.ChangeEvent<HTMLFormElement>) => {
+        const val = e.target.value as string;
+        const tmp = [... selectedColumns] as Array<string>
+        tmp.push(val)
+        setSelectedColumns(tmp)
+    }
+    
+    console.log(selectedCountries)
+    console.log(columnOptions)
     return (
-        <Flex justifyContent="center" m={5}>
-            <table>
-                <thead>
-                    <tr>
-                        {Object.keys(input_example[0]).map((key) => (
-                            <th>{key}</th>
-                        ))}
-                        <th> <Button bg="gray.700"> Add Variable </Button> </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr> 
-                        {Object.values(input_example[0]).map((value) => (
-                            <td>{value}</td>
-                        ))}
-                        <td> - </td>
-                    </tr>
-                </tbody>
-            </table>
-        </Flex>
+        <div>
+            <Flex justifyContent="center" m={5}>
+
+                <table>
+                    <thead>
+                        <tr>
+                            {Object.keys(input_example[0]).map((key) => (
+                                <th>{key}</th>
+                            ))}
+                            <th> 
+                                <Select placeholder='Add Comparison' bg="lightgray" color="black" onChange={handleSelectColumn} variant='filled'>
+                                    {columnOptions.map((col) => (
+                                        <option key={col} value={col}>{col}</option>
+                                    ))}
+                                </Select>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr> 
+                            {Object.values(input_example[0]).map((value) => (
+                                <td>{value}</td>
+                            ))}
+                            <td> - </td>
+                        </tr>
+                        <tr>
+                            <Select placeholder='Select Country' bg="white" color="black" onChange={handleSelectCountry}>
+                                {countries.map((country) => (
+                                    <option key={country['Code']} value={country['Code']}>{country['Country']}</option>
+                                ))}
+                            </Select>
+                        </tr>
+                    </tbody>
+                </table>
+            </Flex>
+        </div>
     )
 }
 
