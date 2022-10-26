a92cc5eb7bc580ec5c9c0d33747fe79e69800125
Matthew Hibbs <89611778+matthibbs7@users.noreply.github.com>
2022-10-26 12:45:22 -0400

    Added dashboard and logic

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next'
 import { Flex, Text } from '@chakra-ui/react'
+import Dashboard from '../components/Dashboard/Dashboard'
 
 const Home: NextPage = () => {
   return (
     <Flex justifyContent='center'>
       <Flex width="100%" maxWidth="1200px" bgColor="#212427" flexDirection='column'>
-        <Text color="white">Test</Text>
+        <Dashboard />
       </Flex>
     </Flex>
   )
